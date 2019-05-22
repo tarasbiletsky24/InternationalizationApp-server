fdf217c55f6bd5ae4bcc2f1d9138159f98860341
Taras Biletskyi <taras.biletsky24@gmail.com>
2019-05-22 11:54:18 +0300

    adding functionality

--- InternationalizationApp/Controllers/UserController.cs
+++ InternationalizationApp/Controllers/UserController.cs
@@ -3,35 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Routing;
 
 namespace InternationalizationApp.Controllers
 {
-    public class ValuesController : ApiController
+    public class UserController : ApiController
     {
-        // GET api/values
-        public IEnumerable<string> Get()
+        [HttpGet]
+        [Route("api/login/{email}/{password}")]
+        public bool Login([FromUri]string email, [FromUri]string password)
         {
-            return new string[] { "value1", "value2" };
+            if ((email != null) && (password != null)) return true;
+                else return false;
         }
 
-        // GET api/values/5
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
         public void Delete(int id)
         {
         }
